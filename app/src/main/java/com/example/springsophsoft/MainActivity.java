d5d688988c99d50aca0579c919623a130746f580
i-dont-like-it <zhibin.he1@snhu.edu>
2020-02-09 23:41:22 -0500

    second page GUI done

--- app/src/main/java/com/example/springsophsoft/MainActivity.java
+++ app/src/main/java/com/example/springsophsoft/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.springsophsoft;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+private Button regisiter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        regisiter = (Button) findViewById(R.id.regisiter);
+
+        regisiter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Register();
+            }
+        });
+    }
+
+    public void Register()
+    {
+        Intent intent = new Intent(this, Signup.class);
+        startActivity(intent);
     }
 }
