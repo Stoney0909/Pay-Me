52da226297a09d1e16db1e97667794b1ea8ca776
Stoney0909 <49493288+Stoney0909@users.noreply.github.com>
2020-02-12 00:43:33 -0500

    Did some verfication.

--- app/src/main/java/com/example/springsophsoft/Signup.java
+++ app/src/main/java/com/example/springsophsoft/Signup.java
@@ -3,12 +3,58 @@ package com.example.springsophsoft;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.example.springsophsoft.helper.Databasehelper;
 
 public class Signup extends AppCompatActivity {
+
+    private static String TAG = "Signup";
+    Databasehelper mDatabasehelper;
+    private EditText password, username, email, confirmPassword;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        btnRegister = (Button) findViewById(R.id.signup);
+        password = (EditText) findViewById(R.id.passwordText);
+        confirmPassword = (EditText) findViewById(R.id.confirmPassword);
+        username = (EditText) findViewById(R.id.Username);
+        email = (EditText) findViewById(R.id.email);
+        mDatabasehelper = new Databasehelper(this);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if (password.length() == 0) {
+                    toastMessage("You must enter something in the Password.");
+                }
+                else if (username.length() == 0){
+                    toastMessage("You must enter something in the Username.");
+                }
+                else if (email.length() == 0){
+                    toastMessage("You must enter something in the Email.");
+                }
+                else if (!password.getText().toString().equals(confirmPassword.getText().toString())){
+                    toastMessage("Passwords do not match.");
+                }
+                else{
+//                    mDatabasehelper.addData();
+                    toastMessage("Success!");
+                }
+
+            }
+        });
+
+    }
+    private void toastMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
