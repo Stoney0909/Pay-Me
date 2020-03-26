a24d00e8d837bea2cb3af1d7b944bc0d3568dc23
Stoney0909 <49493288+Stoney0909@users.noreply.github.com>
2020-03-25 23:06:31 -0400

    got the single list done did some work on the homepage

--- app/src/main/java/com/example/springsophsoft/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/springsophsoft/ui/home/HomeFragment.java
@@ -1,13 +1,11 @@
 package com.example.springsophsoft.ui.home;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,11 +14,7 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.springsophsoft.Helper.Databasehelper;
 import com.example.springsophsoft.Helper.TransactionHelper;
-import com.example.springsophsoft.HomePage;
 import com.example.springsophsoft.R;
-import com.example.springsophsoft.ui.signUpAndLogIn.LogIn;
-
-import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
@@ -31,37 +25,33 @@ public class HomeFragment extends Fragment {
         homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-        Button recievedbtn = (Button)root.findViewById(R.id.recievedBtn);
-        Button sentbtn = (Button)root.findViewById(R.id.sentBtn);
+        Button transactoionsbtn = (Button)root.findViewById(R.id.transactbtn);
         TextView balance = (TextView)root.findViewById(R.id.balanceTextView);
         Databasehelper databasehelper = new Databasehelper(getActivity());
         String amount = "$" + databasehelper.getBalance();
         balance.setText(amount);
 
-        recievedbtn.setOnClickListener(new View.OnClickListener() {
+        transactoionsbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                recieved();
+                transaction();
             }
         });
-        sentbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sent();
-            }
-        });
+        Databasehelper db = new Databasehelper(getActivity());
+        TextView nameTextView = (TextView)root.findViewById(R.id.nameTextView);
+        String name = db.getFirstName() + " " + db.getLastName();
+        nameTextView.setText(name);
+
+
 
 
         return root;
     }
-private void recieved(){
-    Intent intent = new Intent(getActivity(), Recieved.class);
+private void transaction(){
+    Intent intent = new Intent(getActivity(), wholeTransactionList.class);
     startActivity(intent);
 }
-private void sent(){
-        Intent intent = new Intent(getActivity(), Sent.class);
-        startActivity(intent);
-    }
+
 
 
 
