fd0c56157e01fba8b1d3305e14389e9118d05c90
Oumar25 <54920513+Oumar25@users.noreply.github.com>
2020-04-07 19:26:36 -0400

    Working on the swipe

--- app/src/main/java/com/example/springsophsoft/historyMessage.java
+++ app/src/main/java/com/example/springsophsoft/historyMessage.java
@@ -1,8 +1,25 @@
 package com.example.springsophsoft;
 
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -11,6 +28,10 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.example.springsophsoft.Helper.TransactionHelper;
 import com.example.springsophsoft.R;
 import com.example.springsophsoft.ui.Notification.NotificationViewModel;
@@ -21,19 +42,21 @@ import com.example.springsophsoft.ui.signUpAndLogIn.LogIn;
 import java.util.ArrayList;
 import java.util.Collections;
 
+
+
 public class historyMessage extends AppCompatActivity {
 
-
-    private ListView mListView;
-
+    private static final String TAG = "historyMessage";
+    private SwipeMenuListView mListView;
+    TransactionHelper mydb;;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+        mydb = new TransactionHelper(this);
 
 
-
-        mListView = (ListView) findViewById(R.id.sentListView);
+        mListView = (SwipeMenuListView) findViewById(R.id.sentListView);
 
         final TransactionHelper transactiondb = new TransactionHelper(this);
 
@@ -64,23 +87,87 @@ public class historyMessage extends AppCompatActivity {
 
         NotificationListAdapter adapter = new NotificationListAdapter(listData, this);
         mListView.setAdapter(adapter);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//
+//                String name = listData.get(position).getRecieverid();
+//                String amount= listData.get(position).getSenderid();
+////                toastMessage(name);
+////                toastMessage(amount);
+//                open(name,amount);
+////                toastMessage(val);
+////                String text = userlist.getItemAtPosition(position).toString();
+////                Intent intent = new Intent(ge, Accept_Decline.class);
+////                intent.putExtra("Person_SendingTo",text);
+////                startActivity(intent);
+//            }
+//        });
+
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
 
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void create(SwipeMenu menu) {
+                // create "open" item
+                SwipeMenuItem openItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                openItem.setBackground(new ColorDrawable(Color.rgb(0x00, 0x66,
+                        0xff)));
+                // set item width
+                openItem.setWidth(170);
+                // set item title
+                openItem.setTitle("Send");
+                // set item title fontsize
+                openItem.setTitleSize(18);
+                // set item title font color
+                openItem.setTitleColor(Color.WHITE);
+                // add to menu
+                menu.addMenuItem(openItem);
 
-                String name = listData.get(position).getRecieverid();
-                String amount= listData.get(position).getSenderid();
-//                toastMessage(name);
-//                toastMessage(amount);
-                open(name,amount);
-//                toastMessage(val);
-//                String text = userlist.getItemAtPosition(position).toString();
-//                Intent intent = new Intent(ge, Accept_Decline.class);
-//                intent.putExtra("Person_SendingTo",text);
-//                startActivity(intent);
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                // set item width
+                deleteItem.setWidth(170);
+                // set a icon
+                deleteItem.setIcon(R.drawable.ic_denied);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
+        mListView.setMenuCreator(creator);
+
+        mListView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                switch (index) {
+                    case 0:
+                        Log.d(TAG, "onMenuItemClick: clicked item " + index);
+                        String name = listData.get(position).getRecieverid();
+                        String amount= listData.get(position).getSenderid();
+                        Intent intent = new Intent(historyMessage.this, SendMoney.class);
+                        intent.putExtra("Person_SendingTo",name);
+                        startActivity(intent);
+                        mydb.Delete(name,amount);
+                        break;
+                    case 1:
+                        Log.d(TAG, "onMenuItemClick: clicked item " + index);
+                        String name2 = listData.get(position).getRecieverid();
+                        String amount2= listData.get(position).getSenderid();
+                        toastMessage("You successfully denied");
+                        mydb.Delete(name2,amount2);
+                        break;
+                }
+                // false : close the menu; true : not close the menu
+                return false;
             }
         });
+
     }
     public void toastMessage(String message){
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
